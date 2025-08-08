f6a17616e0fb68f83ebafa80777ba5e547b9fa51
vaibhav Bansal <vaibhavbnsl45@gmail.com>
2025-08-08 17:33:53 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express=require("express")
+const cors=require("cors")
+const dotenv=require("dotenv");
+const genContent=require("./routes/chatbot")
+
+dotenv.config();
+const port=process.env.PORT || 5000
+
+const app=express();
+
+app.use(cors({
+  origin: 'http://localhost:5173', // frontend URL
+  credentials: true,
+}));
+app.use(express.json());
+app.use("/", genContent)
+app.use("/reset", genContent )
+
+
+
+app.listen(port,()=> console.log(`server is running on port${port}`))
---
+++ frontend/src/axiousclient.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// Create an Axios instance
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+
+  withCredentials:true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosClient;
